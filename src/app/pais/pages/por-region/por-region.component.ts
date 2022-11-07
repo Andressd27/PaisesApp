a5b0a0ae35cbfe3a471f2c77035a6ca69497b2ad
Andres Duran <andurand27@gmail.com>
2022-11-07 12:59:15 -0500

    Optimizar las peticiones HTTP - incompleto, se estalló

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -12,15 +14,25 @@ import { Component } from '@angular/core';
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
-  regionActiva: string = '';
+  regiones: string[] = [ 'EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC']
+  regionActiva: string = '';  
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor( private paisService: PaisService ) { }
+
+  getClassCSS( region: string ): string {
+    return ( region === this.regionActiva ) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
 
   activarRegion( region: string ) {
-    this.regionActiva = region;
 
-    // TODO: hacer el llamado al servicio
+    if ( region === this.regionActiva ) { return; }
+    
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion( region )
+      .subscribe( paises => this.paises = paises );
   }
 
 }
