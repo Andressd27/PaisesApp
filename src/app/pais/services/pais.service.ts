dc33d6e1ef52ef18b9abcdea15e9af0314f5e9ea
Andres Duran <andurand27@gmail.com>
2022-11-07 13:43:20 -0500

    Animaciones de CSS

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -19,12 +19,12 @@ export class PaisService {
 
   buscarPais ( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>( url, { params: this.httpParams } );
+    return this.http.get<Country[]>( url);
   }
 
   buscarCapital ( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>( url, { params: this.httpParams } );
+    return this.http.get<Country[]>( url );
   }
 
   getPaisPorAlpha ( id: string ): Observable<Country> {
